fedf61763e49c46b17eb75d3c7734a4452f8fd78
Aarvi Prasad <welcome@WELCOMEs-MacBook-Pro.local>
2021-03-16 08:35:27 +0530

    2 Section are done for RE

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
-$('.slick-slider').slick({
+(function ($) {
+  "use strict";
+
+  $('.slick-slider').slick({
     dots: true,
     infinite: true,
     speed: 300,
     slidesToShow: 1,
-    adaptiveHeight: true
+    adaptiveHeight: true,
+    arrows: false,
   });
+  new WOW().init();
+  
+
+})(jQuery);
